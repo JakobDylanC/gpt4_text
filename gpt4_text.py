a4a231e82b602577c6e722a23b0c2e3c33fa2f3c
JakobDylanC <jakobdylanc@gmail.com>
2023-05-11 22:22:07 -0400

    cleaning up

--- gpt4_text.py
+++ gpt4_text.py
@@ -14,16 +14,24 @@ twilio_client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH
 app = Flask(__name__)
 user_sessions = {}
 
-class MessageNode:
-    def __init__(self, message):
-        self.message = message
-        self.tokens = gpt4_functions.count_tokens(message)
+class MsgNode:
+    def __init__(self, msg):
+        self.msg = msg
+        self.tokens = gpt4_functions.count_tokens(msg)
 
 class UserSession:
     def __init__(self):
         self.lock = asyncio.Lock()
         self.message_history = []
         self.total_tokens = 0
+
+    def append_to_history(self, msg_node):
+        self.message_history.append(msg_node)
+        self.total_tokens += msg_node.tokens
+    
+    def pop_from_history(self, index):
+        popped_msg_node = self.message_history.pop(index)
+        self.total_tokens -= popped_msg_node.tokens
 
 def send_sms(body, from_number, to_number):
     twilio_client.messages.create(body=body, from_=from_number, to=to_number)
@@ -37,26 +45,23 @@ async def receive_sms():
         user_session = user_sessions.setdefault(from_number, UserSession())
         
         async with user_session.lock:
-            new_msg = MessageNode({"role": "user", "content": user_prompt_content})
-            user_session.message_history.append(new_msg)
-            user_session.total_tokens += new_msg.tokens
+            user_msg = MsgNode({"role": "user", "content": user_prompt_content})
+            user_session.append_to_history(user_msg)
 
             current_date = datetime.now().strftime("%B %d, %Y")
             system_prompt_content = f"{os.environ['CUSTOM_SYSTEM_PROMPT']}\nKnowledge cutoff: Sep 2021. Current date: {current_date}"
-            system_prompt = MessageNode({"role": "system", "content": system_prompt_content})
+            system_prompt = MsgNode({"role": "system", "content": system_prompt_content})
 
             while user_session.total_tokens + system_prompt.tokens > gpt4_functions.MAX_PROMPT_TOKENS:
                 if len(user_session.message_history) > 1:
-                    removed_msg = user_session.message_history.pop(0)
-                    user_session.total_tokens -= removed_msg.tokens
+                    user_session.pop_from_history(0)
                 else:
                     send_sms("Sorry, an error occurred. Please try again.", os.environ["TWILIO_PHONE_NUMBER"], from_number)
                     return
 
             gpt_response = await gpt4_functions.generate_response(system_prompt.message, [msg.message for msg in user_session.message_history])
-            response_node = MessageNode({"role": "assistant", "content": gpt_response})
-            user_session.message_history.append(response_node)
-            user_session.total_tokens += response_node.tokens
+            response_msg = MsgNode({"role": "assistant", "content": gpt_response})
+            user_session.append_to_history(response_msg)
 
             response_chunks = gpt4_functions.split_response(gpt_response, 960)
             for chunk in response_chunks:
